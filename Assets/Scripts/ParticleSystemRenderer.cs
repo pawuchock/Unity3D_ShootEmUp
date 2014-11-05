c8775076f05ea316280f7a5a6fb9c284a35b0b3c
Pawuchock <vitertm@gmail.com>
2014-11-05 17:46:21 +0200

    qwed

---
+++ Assets/Scripts/ParticleSystemRenderer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSystemRenderer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//particleSystem.renderer.sortingLayerName = "Particles";
+		//ParticleSystem.renderer
+	}	
+}
